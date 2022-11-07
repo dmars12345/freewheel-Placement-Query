056b6f9ea77ebc82ac87a74c839837f9a8e8b26a
dmars12345 <105124496+dmars12345@users.noreply.github.com>
2022-11-07 10:30:13 -0500

    Delete docker.sh

--- docker.sh
+++
@@ -1,5 +0,0 @@
-docker build -t api:query .
-docker run -d api:query
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin AWSID.dkr.ecr.us-east-1.amazonaws.com
-docker tag api:query AWSID.dkr.ecr.us-east-1.amazonaws.com/placement-query:placement-query
-docker push AWSID.dkr.ecr.us-east-1.amazonaws.com/placement-query:placement-query
