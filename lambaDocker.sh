f983006a44c5adddb3012c776c40701e24a69fee
dmars12345 <105124496+dmars12345@users.noreply.github.com>
2022-11-07 10:30:22 -0500

    Delete lambaDocker.sh

--- lambaDocker.sh
+++
@@ -1,5 +0,0 @@
-docker build -t api:lambdaplacement-queryquery .
-docker run -d api:lambdaplacement-queryquery
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin AWSID.dkr.ecr.us-east-1.amazonaws.com
-docker tag api:lambdaplacement-queryquery AWSID.dkr.ecr.us-east-1.amazonaws.com/placement-query:lambda-function
-docker push AWSID.dkr.ecr.us-east-1.amazonaws.com/placement-query:lambda-function
